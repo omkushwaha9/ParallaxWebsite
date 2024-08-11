1844c014b7c117a14a31d45329b4518820da1d93
omkushwaha9 <omkushwaha2005@gmail.com>
2024-08-11 14:00:00 +0530

    Completed

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let text = document.getElementById('text');
 let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
+let hill3 = document.getElementById('hill3');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    text.style.marginTop = value * 2.5 +'px'; 
-    leaf.style.top = value * -1.5 +'px'; 
-    leaf.style.left = value * 1.5 +'px'; 
-    hill5.style.left = value * 1.5 +'px'; 
-    hill4.style.left = value * -1.5 +'px'; 
-    hill1.style.top = value * 1 +'px'; 
-} )
+    text.style.marginTop = value * 2.5 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+   // hill1.style.top = value * 1 + 'px';
+})
